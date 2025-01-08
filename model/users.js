bb7dd5c8183f489cfdd2aed3a50cdf622e259b3c
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-08 23:20:40 +0530

    fresh

---
+++ model/users.js
@@ -0,0 +1,27 @@
+const {Schema, model} = require('mongoose');
+
+const users = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["student", "instructor", "admin"],
+        default: "student"
+    }
+})
